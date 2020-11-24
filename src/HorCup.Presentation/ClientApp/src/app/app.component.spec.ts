81d7986cf293c7a1604e25440e015c46f7fc1005
Artem Horbunov <gorbunowartemwork@gmail.com>
2020-11-24 10:51:27 +0200

    Initial UI setup

--- src/HorCup.Presentation/ClientApp/src/app/app.component.spec.ts
+++ src/HorCup.Presentation/ClientApp/src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ClientApp'`, () => {
+  it(`should have as title 'hor-cup'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ClientApp');
+    expect(app.title).toEqual('hor-cup');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ClientApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'hor-cup app is running!'
+    );
   });
 });
