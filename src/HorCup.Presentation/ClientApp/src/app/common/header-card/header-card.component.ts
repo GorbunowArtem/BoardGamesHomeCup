e82918ce376361b4b47522b0b5b4a1f5e45176c7
Artem Horbunov <gorbunowartemwork@gmail.com>
2020-12-18 21:12:54 +0200

    Added header with navigation

---
+++ src/HorCup.Presentation/ClientApp/src/app/common/header-card/header-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'hc-header-card',
+  templateUrl: './header-card.component.html',
+  styleUrls: ['./header-card.component.scss']
+})
+export class HeaderCardComponent {
+  @Input()
+  text: string = '';
+
+  public constructor(private location: Location) {}
+
+  public goBack() {
+    this.location.back();
+  }
+}
