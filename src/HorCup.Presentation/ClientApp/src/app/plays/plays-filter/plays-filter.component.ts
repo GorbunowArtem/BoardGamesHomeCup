77af746abd36bfd7f872f08ded5d46e5b62260d2
Artem Horbunov <gorbunowartemwork@gmail.com>
2021-01-03 21:08:23 +0200

    Added players selection

--- src/HorCup.Presentation/ClientApp/src/app/plays/plays-filter/plays-filter.component.ts
+++ src/HorCup.Presentation/ClientApp/src/app/plays/plays-filter/plays-filter.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { debounceTime, map, startWith, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { PlayersService } from 'src/app/players/players.service';
+import { Player } from 'src/app/players/models/player';
+import { SearchPlayersOptions } from 'src/app/players/models/search-players-options';
 
 @Component({
   selector: 'hc-plays-filter',
@@ -8,8 +14,11 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class PlaysFilterComponent implements OnInit {
   public playsFilter: FormGroup;
+  public playersCtrl = new FormControl();
+  public selectedPlayers: Set<Player> = new Set<Player>();
+  public playersOption!: Observable<Player[]>;
 
-  public constructor(private _fb: FormBuilder) {
+  public constructor(private _fb: FormBuilder, private _playersService: PlayersService) {
     this.playsFilter = this._fb.group({
       playersIds: [''],
       gamesIds: [''],
@@ -18,16 +27,49 @@ export class PlaysFilterComponent implements OnInit {
     });
   }
 
-  public fruits: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
-
-  public remove(fruit: string): void {
-    const index = this.fruits.indexOf(fruit);
-
-    if (index >= 0) {
-      this.fruits.splice(index, 1);
-    }
+  public ngOnInit() {
+    this.playersOption = this.playersCtrl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(400),
+      switchMap((searchText) => this.filterPlayers(searchText))
+    );
   }
-  public ngOnInit() {}
+
   public reset() {}
-  public search() {}
+  public search() {
+    console.log(this.playersCtrl.value);
+    console.log(this.selectedPlayers);
+  }
+
+  public remove(player: Player): void {
+    this.selectedPlayers.delete(player);
+  }
+
+  public selected(event: MatAutocompleteSelectedEvent): void {
+    this.selectedPlayers.add(event.option.value);
+    this.playersCtrl.setValue(null);
+  }
+
+  public displayPlayer(player: Player) {
+    return player ? `${player?.firstName} ${player?.lastName}` : '';
+  }
+
+  public get selectedPlayersIds(): string[] {
+    const ids: any[] = [];
+
+    this.selectedPlayers.forEach((p: Player) => {
+      ids.push(p.id);
+    });
+
+    return ids;
+  }
+
+  private filterPlayers(searchText: string | Player): Observable<Player[]> {
+    if (typeof searchText !== 'string') {
+      searchText = '';
+    }
+    return this._playersService
+      .search(new SearchPlayersOptions(10, 0, searchText, this.selectedPlayersIds))
+      .pipe(map((resp) => resp.items));
+  }
 }
