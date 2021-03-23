fa76a37d5e2d31891aa88e91270818ecd3291132
Artem Horbunov <gorbunowartemwork@gmail.com>
2021-03-23 22:20:48 +0200

    Added plays nav bar tests

--- src/HorCup.Presentation/ClientApp/src/app/plays/plays-nav-bar/plays-nav-bar.component.spec.ts
+++ src/HorCup.Presentation/ClientApp/src/app/plays/plays-nav-bar/plays-nav-bar.component.spec.ts
@@ -1,27 +1,78 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatToolbarHarness } from '@angular/material/toolbar/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Subject } from 'rxjs';
+import { PlaysService } from '../plays.service';
 import { PlaysNavBarComponent } from './plays-nav-bar.component';
 
 describe('PlaysNavBarComponent', () => {
-  let component: PlaysNavBarComponent;
   let fixture: ComponentFixture<PlaysNavBarComponent>;
+  let loader: HarnessLoader;
+  let playsServiceMock;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [PlaysNavBarComponent]
+  beforeEach(async () => {
+    playsServiceMock = {
+      searchParamsChangedSubject: new Subject(),
+      search: jasmine.createSpy().and.returnValue({
+        subscribe: () => {
+          return {
+            items: [],
+            total: 1
+          };
+        }
+      }),
+      add: jasmine.createSpy()
+    };
+
+    await TestBed.configureTestingModule({
+      imports: [
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatInputModule,
+        MatBottomSheetModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [PlaysNavBarComponent],
+      providers: [{ provide: PlaysService, useValue: playsServiceMock }]
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(PlaysNavBarComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('in view mode', () => {
+    let toolbar: MatToolbarHarness;
+
+    beforeEach(async () => {
+      toolbar = await loader.getHarness(MatToolbarHarness);
+    });
+
+    it('should have menu button ', async () => {
+      const buttons = await toolbar.getAllHarnesses(MatButtonHarness.with({ text: 'menu' }));
+
+      expect(buttons.length).toBe(1);
+    });
+
+    it('should have search button ', async () => {
+      const buttons = await toolbar.getAllHarnesses(MatButtonHarness.with({ text: 'search' }));
+
+      expect(buttons.length).toBe(1);
+    });
+
+    it('should have filter button ', async () => {
+      const buttons = await toolbar.getAllHarnesses(MatButtonHarness.with({ text: 'filter_list' }));
+
+      expect(buttons.length).toBe(1);
+    });
   });
 });
