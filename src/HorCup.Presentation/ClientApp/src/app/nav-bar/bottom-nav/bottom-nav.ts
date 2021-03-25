e9fd30d871d88de74d60003a1b3f5c186f42a75a
Artem Horbunov <gorbunowartemwork@gmail.com>
2021-03-25 18:16:20 +0200

    Feature/bottom navigation (#82)
    * Moved navigation to bottom bar
    
    * Styled bottom nav
    
    * Renamed bottom-nav
    
    * Replaced Common Js imports
    
    * Added animations

---
+++ src/HorCup.Presentation/ClientApp/src/app/nav-bar/bottom-nav/bottom-nav.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hc-footer',
+  templateUrl: './bottom-nav.html',
+  styleUrls: ['./bottom-nav.scss']
+})
+export class BottomNavComponent {}
--- src/HorCup.Presentation/ClientApp/src/app/players/players.service.ts
+++ src/HorCup.Presentation/ClientApp/src/app/players/players.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Subject } from 'rxjs/internal/Subject';
+import { Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { PagedSearchResponse } from '../common/paged-search-response';
 import { Player } from './models/player';
