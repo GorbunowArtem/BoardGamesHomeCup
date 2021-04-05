fbd5f73a1de828089469138e3bf989b2dd72aa74
Artem Horbunov <gorbunowartemwork@gmail.com>
2021-04-05 23:07:23 +0300

    Fixed styling

--- src/HorCup.Presentation/ClientApp/src/app/players/players.component.ts
+++ src/HorCup.Presentation/ClientApp/src/app/players/players.component.ts
@@ -15,31 +15,10 @@ import { PlayersService } from './players.service';
   styleUrls: ['./players.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PlayersComponent implements OnInit, OnDestroy {
-  public loading: boolean;
-
-  public players: Player[];
-
+export class PlayersComponent {
   public playersSource;
 
-  public totalItems: number;
-
-  public playersCountChangedSubscription!: Subscription;
-
-  private readonly _playersPerPage: number;
-
-  private _searchOptions: SearchPlayersOptions;
-
-  public constructor(
-    private _dialog: MatDialog,
-    private _playersService: PlayersService,
-    public dialog: MatDialog
-  ) {
-    this.players = [];
-    this.totalItems = 0;
-    this.loading = false;
-    this._searchOptions = new SearchPlayersOptions();
-    this._playersPerPage = 15;
+  public constructor(private _dialog: MatDialog, private _playersService: PlayersService) {
     this.playersSource = new PlayerSource(_playersService);
   }
 
@@ -49,37 +28,15 @@ export class PlayersComponent implements OnInit, OnDestroy {
     });
   }
 
-  public ngOnInit() {
-    this.search();
-    this.playersCountChangedSubscription = this._playersService.stateChanged().subscribe(() => {
-      this.players = [];
-      this._searchOptions = new SearchPlayersOptions();
-      this.search();
-    });
-  }
-
-  public ngOnDestroy() {
-    this.playersCountChangedSubscription.unsubscribe();
-  }
-
-  public search() {
-    this.loading = true;
-    this._playersService.search(this._searchOptions).subscribe((result) => {
-      this.players.push(...result.items.$values);
-      this.totalItems = result.total;
-      this.loading = false;
-    });
-  }
-
   public edit(player: Player) {
-    this.dialog.open(AddEditPlayerDialogComponent, {
+    this._dialog.open(AddEditPlayerDialogComponent, {
       data: player,
       disableClose: true
     });
   }
 
   public delete(id: string | undefined) {
-    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+    const dialogRef = this._dialog.open(ConfirmationDialogComponent, {
       disableClose: true,
       data: new ConfirmationDialogModel()
     });
@@ -90,24 +47,28 @@ export class PlayersComponent implements OnInit, OnDestroy {
       }
     });
   }
-
-  private loadMore() {
-    if (this.totalItems > this.players.length) {
-      this._searchOptions.take += this._searchOptions.take;
-      this._searchOptions.skip = this._searchOptions.take - this._playersPerPage;
-
-      this.search();
-    }
-  }
 }
 
 export class PlayerSource extends DataSource<Player> {
+  private _loading = false;
   private _cachedPlayers = Array.from<Player>({ length: 0 });
   private _dataStream = new BehaviorSubject<Player[]>(this._cachedPlayers);
   private _subscription = new Subscription();
   private _searchOptions = new SearchPlayersOptions();
   private _pageSize = 15;
   private _lastPage = 0;
+  private _total = 0;
+  private _playersCountChangedSubscription!: Subscription;
+
+  public constructor(private _playersService: PlayersService) {
+    super();
+    this.search();
+    this._playersCountChangedSubscription = _playersService.stateChanged().subscribe(() => {
+      this._cachedPlayers = [];
+      this._searchOptions = new SearchPlayersOptions();
+      this.search();
+    });
+  }
 
   public connect(collectionViewer: CollectionViewer): Observable<Player[]> {
     this._subscription.add(
@@ -116,31 +77,37 @@ export class PlayerSource extends DataSource<Player> {
 
         if (currentPage > this._lastPage) {
           this._lastPage = currentPage;
-          this._searchOptions.take += this._pageSize;
           this._searchOptions.skip = this._pageSize * this._lastPage;
-          this.searchPlayers();
+          this.search();
         }
       })
     );
     return this._dataStream;
   }
-
-  public constructor(private _playerService: PlayersService) {
-    super();
-    this.searchPlayers();
-  }
   public disconnect(): void {
     this._subscription.unsubscribe();
+    this._playersCountChangedSubscription.unsubscribe();
   }
 
   private _getPageForIndex(index: number): number {
     return Math.floor(index / this._pageSize);
   }
 
-  private searchPlayers() {
-    this._playerService.search(this._searchOptions).subscribe((players) => {
+  private search() {
+    this._loading = true;
+    this._playersService.search(this._searchOptions).subscribe((players) => {
+      this._total = players.total;
       this._cachedPlayers = this._cachedPlayers.concat(players.items.$values);
       this._dataStream.next(this._cachedPlayers);
+      this._loading = false;
     });
+  }
+
+  public get total() {
+    return this._total;
+  }
+
+  public get loading() {
+    return this._loading;
   }
 }
